cb08eb8d8c1291901786dabbdc9cb3441688fb27
kuu12 <maaail@hotmail.com>
2018-12-24 15:59:36 +0800

    Update font-size.js

--- src/font-size.js
+++ src/font-size.js
@@ -1,76 +1,72 @@
-export default createFontSize;
-
 /**
  * Create a series of font size styles.
- * For example, execute in console:
- *      createFontSize([320, 375, 411, 667, 750], [42.6666, 100], 5);
+ * __For example, execute in console:__
  *
- * Outputs:
- *      "@media (min-width: 320.0000px) { html { font-size: 42.6666px; } }
- *      @media (min-width: 375.0000px) { html { font-size: 49.9999px; } }
- *      @media (min-width: 406.0000px) { html { font-size: 54.1333px; } }
- *      @media (min-width: 411.0000px) { html { font-size: 54.7999px; } }
- *      @media (min-width: 492.0000px) { html { font-size: 65.6000px; } }
- *      @media (min-width: 578.0000px) { html { font-size: 77.0666px; } }
- *      @media (min-width: 664.0000px) { html { font-size: 88.5333px; } }
- *      @media (min-width: 667.0000px) { html { font-size: 88.9333px; } }
- *      @media (min-width: 750.0000px) { html { font-size: 100.0000px; } }"
+ *     FontSize([320, 375, 411, 667, 750], 1, 20);
  *
- * @param       {Array}         screens         An array that contains at least 2 element.
+ * __Outputs:__
+ *
+ *     "@media (min-width: 320.0000px) { html { font-size: 0.4267px; } }
+ *     @media (min-width: 340.0000px) { html { font-size: 0.4533px; } }
+ *     @media (min-width: 360.0000px) { html { font-size: 0.4800px; } }
+ *     @media (min-width: 375.0000px) { html { font-size: 0.5000px; } }
+ *     @media (min-width: 395.0000px) { html { font-size: 0.5267px; } }
+ *     @media (min-width: 411.0000px) { html { font-size: 0.5480px; } }
+ *     @media (min-width: 431.0000px) { html { font-size: 0.5747px; } }
+ *     @media (min-width: 451.0000px) { html { font-size: 0.6013px; } }
+ *     @media (min-width: 471.0000px) { html { font-size: 0.6280px; } }
+ *     @media (min-width: 491.0000px) { html { font-size: 0.6547px; } }
+ *     @media (min-width: 511.0000px) { html { font-size: 0.6813px; } }
+ *     @media (min-width: 531.0000px) { html { font-size: 0.7080px; } }
+ *     @media (min-width: 551.0000px) { html { font-size: 0.7347px; } }
+ *     @media (min-width: 571.0000px) { html { font-size: 0.7613px; } }
+ *     @media (min-width: 591.0000px) { html { font-size: 0.7880px; } }
+ *     @media (min-width: 611.0000px) { html { font-size: 0.8147px; } }
+ *     @media (min-width: 631.0000px) { html { font-size: 0.8413px; } }
+ *     @media (min-width: 651.0000px) { html { font-size: 0.8680px; } }
+ *     @media (min-width: 667.0000px) { html { font-size: 0.8893px; } }
+ *     @media (min-width: 687.0000px) { html { font-size: 0.9160px; } }
+ *     @media (min-width: 707.0000px) { html { font-size: 0.9427px; } }
+ *     @media (min-width: 727.0000px) { html { font-size: 0.9693px; } }
+ *     @media (min-width: 747.0000px) { html { font-size: 0.9960px; } }
+ *     @media (min-width: 750.0000px) { html { font-size: 1.0000px; } }"
+ *
+ *
+ * @param       {Array}         screenList      An array that contains at least 2 element.
  *                                              The head element is the lower boundary of screen size that
  *                                              you would like to match, and the last element is the upper boundary.
  *                                              Also, you can specific some familiar screen sizes, telling this function
  *                                              these screens need to be specific exact font sizes.
  *
- * @param       {Array}         fontSizes       An array that contains 2 element. The first is minimum font size,
- *                                              the second is maximal font size.
+ * @param       {number = 1}    sizeOf750px
  *
- * @param       {number = 20}   divide          Between the minimum screen and maximal screen, how many screen that
- *                                              you want to automatically create and specific a adaptive font size.
+ * @param       {number = 20}   step
  *
  * @return      {string}                        Styles. Copy and paste them into your project.
  */
 
-function createFontSize(
-    screens,
-    [fontSizeLower, fontSizeUpper],
-    divide = 20,
-) {
-    const screenLower = screens[0];
-    const screenUpper = screens[screens.length - 1];
-
-    const calcScreen = (ignored, index) =>
-        screenLower +
-        (screenUpper - screenLower) *
-        (index / divide);
-
-    const calcFontSize = screen =>
-        fontSizeLower +
-        (fontSizeUpper - fontSizeLower) *
-        (screen - screenLower) /
-        (screenUpper - screenLower);
-
-    return Array
-        .from(
-            new Set(
-                Array(divide + 1)
-                    .fill(undefined)
-                    .map(calcScreen)
-                    .concat(screens)
-            )
-        )
-        .sort((a, b) => a - b)
+const FontSize = (screenList, sizeOf750px = 1, step = 20) =>
+    screenList
+        .map((screen, index) => {
+            const set = [];
+            do {
+                set.push(screen);
+                screen += step;
+            } while (
+                index < screenList.length &&
+                screen <= screenList[index + 1]
+            );
+            return set;
+        })
+        .reduce((all, set) => [...all, ...set], [])
         .map(screen => [
             screen,
-            calcFontSize(screen),
-        ])
-        .map(([screen, fontSize]) => [
-            screen.toFixed(4),
-            fontSize.toFixed(4),
+            (screen / 750 * sizeOf750px).toFixed(4),
         ])
         .map(([screen, fontSize]) =>
             `@media (min-width: ${screen
             }px) { html { font-size: ${fontSize}px; } }`
         )
         .join('\n');
-}
+
+export default FontSize;
